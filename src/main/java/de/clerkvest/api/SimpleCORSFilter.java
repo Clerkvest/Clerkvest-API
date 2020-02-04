9e98e467b391ba7bbdec2d71ecac2e41b013efd3
Qu1oX <danny1996@gmx.net>
2020-02-04 22:55:03 +0100

    Cors filter as bean

--- src/main/java/de/clerkvest/api/SimpleCORSFilter.java
+++ src/main/java/de/clerkvest/api/SimpleCORSFilter.java
@@ -1,17 +1,27 @@
 package de.clerkvest.api;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
-public class SimpleCORSFilter implements WebMvcConfigurer {
+public class SimpleCORSFilter {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE")
-                .allowedHeaders("Origin", "Content-Type", "Accept", "X-Requested-With", "remember-me", "Authorization");
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // Don't do this in production, use a proper list  of allowed origins
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
+
 }
