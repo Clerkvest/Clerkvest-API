39a011c2c4822a8a71a22359dd5eed319bd944cf
Voxen4 <m.keller490@gmail.com>
2020-02-08 13:03:07 +0100

    Added Swagger Auto Doc Validation
    Signed-off-by: Voxen4 <m.keller490@gmail.com>

--- src/main/java/de/clerkvest/api/SpringFoxConfig.java
+++ src/main/java/de/clerkvest/api/SpringFoxConfig.java
@@ -2,15 +2,18 @@ package de.clerkvest.api;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
 @EnableSwagger2WebMvc
-//@Import(SpringDataRestConfiguration.class)
+@Import({BeanValidatorPluginsConfiguration.class, SpringDataRestConfiguration.class})
 public class SpringFoxConfig {
     @Bean
     public Docket api() {
