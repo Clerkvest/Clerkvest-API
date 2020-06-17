ad3fde8c64785f726df37abd28a8cc741374071a
Michael Keller <m.keller490@gmail.com>
2020-06-18 01:57:56 +0200

    Revert "Added Root Cause to TransactionSystemException Logger"
    This reverts commit 4b847b4f29f2099b751c7b64fe063a7b3b2713b6.

--- src/main/java/de/clerkvest/api/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/de/clerkvest/api/exception/RestResponseEntityExceptionHandler.java
@@ -56,7 +56,7 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
                     new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
         }
         String bodyOfResponse = "A Error Occurred: ";
-        return handleExceptionInternal(ex, bodyOfResponse + ((TransactionSystemException) ex).getRootCause(),
+        return handleExceptionInternal(ex, bodyOfResponse + ex.getMessage(),
                 new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
